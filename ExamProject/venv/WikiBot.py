036f1796cabca2bf956d101876875ddd7d2304d4
Syimyk04 <syimykosmonov04@gmail.com>
2024-11-12 20:44:41 +0300

    Update WikiBot.py
    Исправление ошибок поисковика

--- ExamProject/venv/WikiBot.py
+++ ExamProject/venv/WikiBot.py
@@ -1,11 +1,12 @@
 # Подключение библиотек и модулей
 import wikipedia
 import telebot
+import random
 from telebot import types
 from module import check_surname
 
 wikipedia.set_lang('ru')
-bot = telebot.TeleBot('token')
+bot = telebot.TeleBot('Token')
 
 # Запуск бота, приветствие
 @bot.message_handler(commands=['start'])
@@ -24,6 +25,7 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def mess(message):
     word = message.text.strip().lower()
+    ps = '<b>Статья из официальных источников Википедии🌐</b>'
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('Подробнее', url=f'https://ru.wikipedia.org/wiki/{word}'))
     try:
@@ -31,8 +33,13 @@ def mess(message):
     except wikipedia.exceptions.PageError:
         final_mess = ''
         bot.send_message(message.chat.id, f'Не очень вас понял😕\nПопробуйте написать слово или термин')
+    # Если у искомого слова много вариантов
+    except wikipedia.exceptions.DisambiguationError as choice_error:
+        f = random.choice(choice_error.options)
+        final_mess = wikipedia.summary(f)
+        bot.send_message(message.chat.id, f'{final_mess}\n{ps}', reply_markup=markup, parse_mode='html')
     else:
-        bot.send_message(message.chat.id, f'{final_mess}\n<b>Статья из официальных источников Википедии🌐</b>', reply_markup=markup, parse_mode='html')
+        bot.send_message(message.chat.id, f'{final_mess}\n{ps}', reply_markup=markup, parse_mode='html')
 
 
 
